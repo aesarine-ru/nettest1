e509b64f8caba07a22bd9da52d92cbe822c071fd
aesarine-ru <69194155+aesarine-ru@users.noreply.github.com>
2020-09-09 12:40:57 +0300

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Text } from "@quarkly/widgets";
+import { Theme, Image, Text } from "@quarkly/widgets";
 export default (() => {
 	return <Theme theme={theme}>
+		<Image width="64px" height="64px" />
 		<Text>
 			Some text
 		</Text>
